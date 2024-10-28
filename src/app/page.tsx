355050e352a5eb4639c071c5713f845d90623cfe
ZakheerAli <zakheerali@gmail.com>
2024-10-28 13:53:53 +0500

    Normalize line endings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,74 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-full relative flex justify-center  bg-darkBlue ">
+      <nav className="w-[1200px] h-[91px] flex items-center   bg-darkBlue justify-evenly absolute left-24">
+        <div className="w-[187px] h-[58px] flex justify-center items-center  bg-darkBlue">
+          <h3 className="text-[#FFFFFF] font-montserrat text-2xl font-bold">BrandName</h3>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-[815px] h-[58px] flex items-center justify-between  bg-darkBlue">
+          <div className="w-[275px] h-[24px] flex items-center justify-evenly  bg-darkBlue">
+            <a href="#" className="text-[#FFFFFF] text-sm font-bold font-montserrat  hover:border-b-4 hover:border-[#23A6F0] ">Home</a>
+            <a href="#" className="text-[#FFFFFF] text-sm font-bold font-montserrat  hover:border-b-4 hover:border-[#23A6F0]">Product</a>
+            <a href="#" className="text-[#FFFFFF] text-sm font-bold font-montserrat  hover:border-b-4 hover:border-[#23A6F0]">Pricing</a>
+            <a href="#" className="text-[#FFFFFF] text-sm font-bold font-montserrat  hover:border-b-4 hover:border-[#23A6F0]">Contact</a>
+          </div>
+          <div className="w-[189px] h-[52px] flex items-center justify-between  bg-darkBlue">
+            <button className="text-[#FFFFFF] text-sm font-bold font-montserrat hover:text-[#23A6F0] hover:font-extrabold">Login</button>
+            <button className="text-[#FFFFFF] text-sm font-bold rounded-md font-montserrat w-[110px] h-[52px] bg-[#23A6F0] hover:bg-[#FFFFFF] hover:text-[#23A6F0] hover:font-extrabold ">JOIN US</button>
+          </div>
+        </div>
+      </nav>
+      <div className="h-[900px] w-[1046px] flex items-center justify-center  flex-col relative top-[75px]   gap-5  bg-darkBlue">
+        <div className="w-[699px]  bg-darkBlue h-[496px] relative top-0 gap-10 flex justify-center flex-col items-center ">
+          <div className="w-[77px] h-[24px]  bg-darkBlue flex justify-center items-center ">
+            <h5 className="text-[#23A6F0]  font-montserrat text-base font-bold">Welcome</h5>
+          </div>
+          <div className="w-[542px] h-[160px] flex items-center text-center  bg-darkBlue">
+            <h1 className=" text-[#FFFFFF] text-[58px] font-montserrat font-bold tracking-wide ">Selling on the internet like a pro</h1>
+          </div>
+          <div className="w-[536px] h-[60px] flex items-center text-center  bg-darkBlue">
+            <h3 className="text-xl text-[#FFFFFF] font-medium font-montserrat tracking-wide">We know how large objects will act, but things on a
+              small scale just do not act that way.</h3>
+          </div>
+          <div className="w-[365px] h-[52px]  bg-darkBlue flex items-center justify-evenly gap-[10px]">
+            <button className="w-[193px] h-[52px]  text-[#FFFFFF] rounded-md bg-[#23A6F0] font-montserrat text-sm font-bold hover:bg-[#FFFFFF] hover:text-[#23A6F0] hover:font-extrabold hover:border-2 hover:border-[#23A6F0]">Get Quote Now</button>
+            <button className="w-[193px] h-[52px] rounded-md text-[#23A6F0] font-montserrat text-sm font-bold  bg-darkBlue border-2 border-[#23A6F0] hover:font-extrabold hover:border-2 hover:border-[#FFFFFF] hover:text-[#FFFFFF]">Learn More</button>
+          </div>
+        </div>
+        <div className="w-[1046px] h-[292px] flex justify-between bg-darkBlue ">
+          <div className="w-[328px] h-[292px] cursor-pointer px-[35px] flex flex-col py-[40px] gap-5 bg-white hover:h-[300px] hover:w-[335]">
+            <div className="w-[70px] h-[76px] px-[22px] py-[19px] rounded-md bg-[#FFDCD1]"></div>
+            <div className="w-[135px] h-[24px]">
+              <h5 className="text-base font-bold font-montserrat">training Courses</h5>
+            </div>
+            <div className="w-[50px] h-[2px] bg-[#E74040]"></div>
+            <div className="w-[222px] h-[60px]">
+              <p className="text-sm font-normal font-montserrat text-[#737373]">The gradual accumulation of information about atomic and small-scale behaviour...</p>
+            </div>
+          </div>
+          <div className="w-[328px] h-[292px] px-[35px] py-[40px] flex flex-col gap-5 bg-[#FFFFFF] cursor-pointer hover:h-[300px] hover:w-[335]">
+            <div className="w-[70px] h-[76px] bg-[#B9EAA8]  px-[22px] py-[19px] rounded-md"></div>
+            <div className="w-[168px] h-[24px]">
+              <h5 className="text-base font-bold font-montserrat">2,769 online courses</h5>
+            </div>
+            <div className="w-[50px] h-[2px] bg-[#E74040]"></div>
+            <div className="w-[222px] h-[60px]">
+              <p className="text-sm font-normal font-montserrat text-[#737373]">The gradual accumulation of information about atomic and small-scale behaviour...</p>
+            </div>
+          </div>
+          <div className="w-[328px] h-[292px] px-[35px] py-[40px] gap-5 flex flex-col bg-[#23A6F0] cursor-pointer hover:h-[300px] hover:w-[335]">
+            <div className="w-[70px] h-[76px] bg-[#FFFFFF] px-[22px] py-[19px]  rounded-md"></div>
+            <div className="w-[135px] h-[24px]">
+              <h5 className="text-base font-bold font-montserrat text-[#FFFFFF]">training Courses</h5>
+            </div>
+            <div className="w-[50px] h-[2px] bg-[#FFFFFF]"></div>
+            <div className="w-[222px] h-[60px]">
+              <p className="text-sm font-normal font-montserrat text-[#FFFFFF]">The gradual accumulation of information about atomic and small-scale behaviour...</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,22 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        darkBlue: "#252B42",
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      inset: {
+        '-590': '-590px', // Custom negative top position
+        '-720': '-720px', // Custom negative left position
+      },
+      width: {
+        '187':'187px',
+        '1440': '1440px', // Custom width for 1440px
+      },
+      height: {
+        '58':'58px',
+        '1132': '1132px', // Custom height for 1132px
       },
     },
   },
